e3427736b2f61394d4d2a4eda3cebc3d625e6c4a
julierk-keck <138939623+julierk-keck@users.noreply.github.com>
2023-07-13 05:02:25 +0000

    Created individual job page and updated all to use database

--- app.py
+++ app.py
@@ -14,9 +14,14 @@ def list_jobs():
   jobs = load_jobs_from_db()
   return jsonify(jobs)
 
-def show_job(id):
+@app.route("/jobs/<id>")   #define a dynamic route by using <variable>
+def job_page(id):
   job = load_job_from_db(id)
-  return jsonify(job)
+  #return jsonify(job)
+  if not job:
+    return render_template('notFound.html'), 404
+  
+  return render_template('jobApply.html', job=job, company_name="Julie's")
 
 print(__name__)
 
--- database.py
+++ database.py
@@ -13,7 +13,7 @@ engine = create_engine(db_connection_string, connect_args={
 })
     
 def load_jobs_from_db():
-  with engine.connect() as conn:
+  with engine.connect() as conn:  # Use with loop so db closes when loop ends
     result = conn.execute(text("select * from jobs"))
     
     result_dicts = []
@@ -27,7 +27,8 @@ def load_jobs_from_db():
 
 def load_job_from_db(id):
   with engine.connect() as conn:
-    result = conn.execute(text("SELECT * FROM jobs where id = :val"), val=id)
+    result = conn.execute(
+      text(f"SELECT * FROM jobs where id = {id}")) #Python string+var format
     rows = result.all()
     if len(rows) == 0:
       return None
